ebc8cf84cc70c6200a1d60b0b6515e47945c9ac2
theofbraune <theo@rieffel-braune.de>
2022-03-22 11:56:22 +0100

    update the obstacles

--- png2geometry.py
+++ png2geometry.py
@@ -48,23 +48,24 @@ try:
 				dot=pix[x,y]
 				#if(dot>0):
 				#	print('dot value: ', dot)
-				dot//=255
+				#dot//=255
 				if (dot == 0) or (dot == 1):
 					"""
 					flip vertically: obstacle coordinates start
 					at bottom, image coordinates at top
 					"""
 					if(dot==0):
-						#dot = 1
-						pass
+						dot = 1
+						#pass
 					else:
-						#dot=0
-						pass
+						dot=0
+						#pass
 					out.append("%s %s %s"%(x,height-y-1,dot))
 				else:
 					print('dot: ', dot)
 					print("wrong data")
 					raise Exception
+			
 		print("Writing numerical data to geometry file.")
 		outfile=open(geofile, 'w')
 		outfile.write("LX %d\n" %width)
